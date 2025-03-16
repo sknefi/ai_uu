5b742f9f6dd5f041ca57d315ce66cec1ebb46302
sknefi <filip.karika1@gmail.com>
2025-03-16 17:25:32 +0100

    3 - looks like final version

--- p_03/uloha/constants.py
+++ p_03/uloha/constants.py
@@ -1,11 +1,55 @@
 
-PATH0 = 'graphs/small_graph0.col'
-PATH1 = 'graphs/small_graph1.col'
-PATH2 = 'graphs/dsjc250.5.col'
-PATH3 = 'graphs/dsjc500.1.col'
-PATH4 = 'graphs/star_graph.col'
-PATH5 = 'graphs/complete_graph.col'
-MAX_ITER = 1000
-RANDOM_WALK_PROB = 0.3
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+ORANGE = "\033[1;33m"
+YELLOW = "\033[1;34m"
+RESET = "\033[0m"
 
-NUM_COLORS = 30
+PATH0 = 'graphs/small_graph0.col' # licha kruznica
+NUM_COLORS0 = 3
+
+PATH1 = 'graphs/small_graph1.col'
+NUM_COLORS1 = 3
+
+PATH2 = 'graphs/dsjc250.5.col'
+NUM_COLORS2 = 38
+
+PATH3 = 'graphs/dsjc500.1.col'
+NUM_COLORS3 = 12
+
+PATH4 = 'graphs/dsjc500.5.col'
+NUM_COLORS4 = 48
+
+PATH5 = 'graphs/star_graph.col' # hviezda
+NUM_COLORS5 = 2
+
+PATH6 = 'graphs/comple_graph.col'
+NUM_COLORS6 = 2
+
+P = 4
+
+if P == 0:
+    PATH = PATH0
+    NUM_COLORS = NUM_COLORS0
+elif P == 1:
+    PATH = PATH1
+    NUM_COLORS = NUM_COLORS1
+elif P == 2:
+    PATH = PATH2
+    NUM_COLORS = NUM_COLORS2
+elif P == 3:
+    PATH = PATH3
+    NUM_COLORS = NUM_COLORS3
+elif P == 4:
+    PATH = PATH4
+    NUM_COLORS = NUM_COLORS4
+elif P == 5:
+    PATH = PATH5
+    NUM_COLORS = NUM_COLORS5
+elif P == 6:
+    PATH = PATH6
+    NUM_COLORS = NUM_COLORS6
+    
+
+MAX_ITER = 1000
+RANDOM_WALK_PROB = 0.9
--- p_03/uloha/main.py
+++ p_03/uloha/main.py
@@ -3,13 +3,14 @@ from constants import *
 
 def main():
 	# Načítame graf
-	G = readdimacs(PATH2)
+	G = readdimacs(PATH)
 	print("# vrcholov:", G.number_of_nodes(), ";", "# hran:", G.number_of_edges())
 
-	best_coloring, conflicts = hill_climbing_random_walk(G, NUM_COLORS, MAX_ITER, RANDOM_WALK_PROB)
+	coloring, conflicts = hill_climbing_random_walk(G, NUM_COLORS, MAX_ITER, RANDOM_WALK_PROB)
+	
 	print("Najdených konfliktov:", conflicts)
-	print(best_coloring)
-	plot(G, best_coloring)
+	is_coloring(G, coloring)
+	plot(G, coloring)
 
 if __name__ == '__main__':
     main()
