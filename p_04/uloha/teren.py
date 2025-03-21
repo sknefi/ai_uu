5b272385e5a25221dde15adc67ef49fa590cca52
sknefi <filip.karika1@gmail.com>
2025-03-21 23:50:39 +0100

    4 - prednaska DEAP update

--- p_04/uloha/teren.py
+++ p_04/uloha/teren.py
@@ -6,14 +6,15 @@ y = [1.0, 0.2, 0.5, 0.6, 0.2, 0.7, 0.8]
 # teren je seznam realnych cisel mezi 0 a 1, 0.5 je hladina more 
 def plotterrain(t):
 
-    fig, ax = plt.subplots()
+	fig, ax = plt.subplots()
 
-    x = range(len(t))
-    sea = [0.5 for i in range(len(t))]
+	x = range(len(t))
+	sea = [0.5 for i in range(len(t))]
 
-    ax.fill_between(x, sea, color="turquoise")
-    ax.fill_between(x, t, color="sandybrown")
-    ax.axis("off")
-    plt.show()
+	ax.fill_between(x, sea, color="turquoise")
+	ax.fill_between(x, t, color="sandybrown")
+	ax.axis("off")
+	plt.show()
 
-plotterrain(y)
+for x, y in zip(range(10), range(5)):
+    print(x, y)
